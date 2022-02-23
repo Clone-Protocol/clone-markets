9a4e362d5700039b7963eb351af394dfffe2d714
“brian” <quantum34@protonmail.com>
2022-02-23 10:54:31 +0900

    add query client hocs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
+import React, { ReactElement, ReactNode } from 'react'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import QueryProvider from '~/hocs/QueryClient'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
@@ -7,32 +10,34 @@ import Drawer from '~/components/Drawer'
 import Box from '@mui/material/Box';
 import { SnackbarProvider } from 'notistack'
 import { NextPage } from 'next'
-import React, { ReactElement, ReactNode } from 'react'
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <GNB />
-        <Drawer />
+    <QueryProvider>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
+          <GNB />
+          <Drawer />
 
-        <SnackbarProvider maxSnack={3}>
-          <Box
-            component="main"
-            sx={{
-              flexGrow: 1,
-              height: '100vh',
-              overflow: 'auto',
-            }}
-          >
-            {getLayout(<Component {...pageProps} />)}
-          </Box>
-        </SnackbarProvider>
-      </Box>
-    </ThemeProvider>
+          <SnackbarProvider maxSnack={3}>
+            <Box
+              component="main"
+              sx={{
+                flexGrow: 1,
+                height: '100vh',
+                overflow: 'auto',
+              }}
+            >
+              {getLayout(<Component {...pageProps} />)}
+            </Box>
+          </SnackbarProvider>
+        </Box>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryProvider>
   )
 }
 
