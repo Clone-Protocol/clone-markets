cb12b83067a7cd6cd41dab453771fbc3c93355d4
brian <quantum34@protonmail.com>
2022-09-01 01:13:33 +0900

    update GNB

--- src/components/GNB.tsx
+++ src/components/GNB.tsx
@@ -53,7 +53,7 @@ const GNB: React.FC = () => {
 		<>
 			<NavPlaceholder />
 			<StyledAppBar className={navClassName} position="static">
-				<Container maxWidth="xl">
+				<Container maxWidth={false}>
 					<Toolbar disableGutters sx={{ paddingLeft: '10px' }}>
 						<Image src={logoIcon} alt="incept" />
             <MarketTitle>Markets</MarketTitle>
