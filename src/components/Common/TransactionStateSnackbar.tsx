121729d770cc2fa9d25cb95c8bdd2d646b1be08e
brian <quantum34@protonmail.com>
2023-05-28 14:49:12 +0900

    update snackbar

--- src/components/Common/TransactionStateSnackbar.tsx
+++ src/components/Common/TransactionStateSnackbar.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
-import { Box, styled, Typography, Snackbar, CircularProgress } from '@mui/material'
+import { Box, Stack, styled, Typography, Snackbar, CircularProgress } from '@mui/material'
 import SuccessIcon from 'public/images/check-mark-icon.svg'
 import FailureIcon from 'public/images/failure-mark-icon.svg'
-import CloseIcon from 'public/images/close.svg'
+import CloseIcon from 'public/images/close-round.svg'
 import Image from 'next/image'
 import { TransactionState } from '~/hooks/useTransactionState'
 import Slide from '@mui/material/Slide';
@@ -27,34 +27,40 @@ const getTxnURL = (txHash: string) => {
 }
 
 const SuccessFailureWrapper = ({ isSuccess, txHash }: { isSuccess: boolean, txHash: string }) => {
-  return (<Box>
-    <Box mt='10px'><Image src={isSuccess ? SuccessIcon : FailureIcon} width={47} height={47} /></Box>
-    <Box mt='10px'><Typography variant='h7'>Transaction {isSuccess ? 'complete' : 'failed'}</Typography></Box>
-    <Box my='10px' lineHeight='1'>
-      <Typography variant='p' color='#989898'>
-        {isSuccess ? 'You can now access all features.' : 'There was an error. Please try again.'}
-      </Typography>
+  const txStatusColor = isSuccess ? '#00ff99' : '#ff0084'
+  return (<Stack direction='row' gap={2}>
+    <Box mt='10px'><Image src={isSuccess ? SuccessIcon : FailureIcon} width={51} height={51} /></Box>
+    <Box my='6px' lineHeight={1.3}>
+      <Box mt='6px'><Typography variant='p_xlg'>Transaction {isSuccess ? 'complete' : 'failed'}</Typography></Box>
+      {!isSuccess && <Box mt='6px'><Typography variant='p' color='#8988a3'>Something went wrong. Please try again.</Typography></Box>}
+      <Box mb='10px' sx={{ textDecoration: 'underline', color: txStatusColor }}>
+        <a href={getTxnURL(txHash)} target='_blank' rel="noreferrer"><Typography variant='p_sm' color={txStatusColor}>{isSuccess ? 'View Transaction' : 'Check Solana network status'}</Typography></a>
+      </Box>
     </Box>
-    <Box mb='10px' sx={{ textDecoration: 'underline', color: '#258ded' }}><a href={getTxnURL(txHash)} target='_blank' rel="noreferrer"><Typography variant='p' color='#258ded'>View Transaction</Typography></a></Box>
-  </Box>)
+  </Stack>)
 }
 
 const ConfirmingWrapper = ({ txHash, isFocus }: { txHash: string, isFocus: boolean }) => {
   const [longTimeStatus, setLongTimeStatus] = useState<JSX.Element>()
-  const StatusWrap = (<LongTimeStatus><Typography variant='p'>This transaction is taking unusually long. Please check <br /> <a href='https://status.solana.com/' target='_blank' rel="noreferrer">Solana Network status</a></Typography></LongTimeStatus>)
+  const StatusWrap = (<LongTimeStatus><Typography variant='p'>This transaction is taking longer than usual. Please check <br /> <a href='https://status.solana.com/' target='_blank' rel="noreferrer">Solana Network status</a></Typography></LongTimeStatus>)
   setTimeout(() => {
     setLongTimeStatus(StatusWrap)
   }, 15000)
 
   return (
     <ConfirmBoxWrapper className={isFocus ? 'animate__animated animate__shakeX' : ''}>
-      <CircularProgress sx={{ color: '#fff' }} size={23} thickness={8} />
-      <Box mt='10px'><Typography variant='h7'>Confirming transaction</Typography></Box>
-      <Box my='10px' lineHeight={1}><Typography variant='p' color={isFocus ? '#ff8e4f' : '#989898'}>All features are disabled until the transaction is confirmed.
-        <br />Transactions on Solana typically take an average of 5 seconds. </Typography></Box>
-      <Box sx={{ textDecoration: 'underline', color: '#258ded' }}><a href={getTxnURL(txHash)} target='_blank' rel="noreferrer"><Typography variant='p' color='#258ded'>View Transaction</Typography></a></Box>
+      <Stack direction='row' spacing={2}>
+        <CircularProgress sx={{ color: '#ff6cdf' }} size={56} thickness={5} />
+        <Box>
+          <Box><Typography variant='p_xlg'>Confirming transaction...</Typography></Box>
+          <Box my='6px' lineHeight={1.3}>
+            <Box><Typography variant='p' color='#8988a3'>Transactions on Solana typically take an average of 5 seconds. </Typography></Box>
+            <Box sx={{ textDecoration: 'underline', color: '#c4b5fd' }}><a href={getTxnURL(txHash)} target='_blank' rel="noreferrer"><Typography variant='p_sm' color='#c4b5fd'>View Transaction</Typography></a></Box>
+          </Box>
+        </Box>
+      </Stack>
       {longTimeStatus}
-    </ConfirmBoxWrapper>
+    </ConfirmBoxWrapper >
   )
 }
 
@@ -70,13 +76,13 @@ const TransactionStateSnackbar = ({ txState, txHash, open, handleClose }: { txSt
         <Snackbar open={open} autoHideDuration={60000} onClose={txState === TransactionState.PENDING ? () => { } : handleClose}>
           <Box>
             {txState === TransactionState.SUCCESS &&
-              <BoxWrapper>
+              <BoxWrapper sx={{ border: '1px solid #00ff99' }}>
                 <CloseButton onClick={handleClose}><Image src={CloseIcon} /></CloseButton>
                 <SuccessFailureWrapper isSuccess={true} txHash={txHash} />
               </BoxWrapper>
             }
             {txState === TransactionState.FAIL &&
-              <BoxWrapper>
+              <BoxWrapper sx={{ border: '1px solid #ff0084' }}>
                 <CloseButton onClick={handleClose}><Image src={CloseIcon} /></CloseButton>
                 <SuccessFailureWrapper isSuccess={false} txHash={txHash} />
               </BoxWrapper>
@@ -99,13 +105,13 @@ const BackLayer = styled('div')`
   left: 0;
   background: transparent;
 `
-
 const BoxWrapper = styled(Box)`
-  width: 236px;
-  text-align: center;
-  border-radius: 15px;
-  padding: 12px;
-  background: ${(props) => props.theme.boxes.black};
+  width: 419px;
+  display: flex;
+  align-items: center;
+  border-radius: 10px;
+  padding: 12px 17px;
+  background: #080e1c;
 `
 const CloseButton = styled(Box)`
   position: absolute;
@@ -115,17 +121,18 @@ const CloseButton = styled(Box)`
 `
 
 const ConfirmBoxWrapper = styled(Box)`
-  width: 442px;
-  border-radius: 15px;
-  padding: 15px 32px;
-  background: ${(props) => props.theme.boxes.black};
+  width: 419px;
+  border-radius: 10px;
+  padding: 22px 10px;
+  box-shadow: 0 0 32px 4px rgba(167, 108, 242, 0.25);
+  border: solid 1px ${(props) => props.theme.basis.lightSlateBlue};
+  background: ${(props) => props.theme.basis.darkPurple};
 `
 const LongTimeStatus = styled(Box)`
   padding: 12px 18px;
   color: ${(props) => props.theme.palette.warning.main};
-  border: solid 1px ${(props) => props.theme.palette.warning.main};
-  margin-top: 13px;
-  line-height: 0.8;
+  margin-top: 8px;
+  line-height: 1;
   a {
     color: ${(props) => props.theme.palette.warning.main};
     text-decoration: underline;
