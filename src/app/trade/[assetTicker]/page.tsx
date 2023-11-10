03b19dfeeeb39a2d134e9003117ed36ece028d34
brian <quantum34@protonmail.com>
2023-11-10 18:59:43 +0900

    mobile responsive for trading page

--- src/app/trade/[assetTicker]/page.tsx
+++ src/app/trade/[assetTicker]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState, useMemo, useCallback } from 'react'
 import { useRouter } from 'next/navigation'
-import { Box, Stack } from '@mui/material'
+import { Box, Stack, Theme, useMediaQuery } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import MarketDetail from '~/containers/Markets/MarketDetail'
 import TradingBox from '~/containers/Markets/TradingBox'
@@ -9,6 +9,7 @@ import { AssetTickers } from '~/data/assets'
 
 const AssetPage = ({ params }: { params: { assetTicker: string } }) => {
   const router = useRouter()
+  const isMobileOnSize = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'))
   const [assetId, setAssetId] = useState(0)
   const assetTicker = params.assetTicker || AssetTickers.euro
 
@@ -35,8 +36,8 @@ const AssetPage = ({ params }: { params: { assetTicker: string } }) => {
         sx={{
           backgroundColor: '#000',
         }}>
-        <Stack direction='row' gap={1} justifyContent="center">
-          <Box minWidth='750px'>
+        <Stack direction={isMobileOnSize ? 'column' : 'row'} gap={1} justifyContent="center" alignItems={isMobileOnSize ? "center" : ""}>
+          <Box minWidth={isMobileOnSize ? '360px' : '750px'}>
             <MarketDetail assetId={assetId} />
           </Box>
           <Box width='360px'>
@@ -44,6 +45,7 @@ const AssetPage = ({ params }: { params: { assetTicker: string } }) => {
           </Box>
         </Stack>
       </StyledSection>
+      <Box display={isMobileOnSize ? 'block' : 'none'}><ShowTradingBtn>Show Trading</ShowTradingBtn></Box>
     </div>
   )
 }
@@ -55,6 +57,25 @@ const StyledSection = styled('section')`
 	${(props) => props.theme.breakpoints.down('md')} {
 		padding: 50px 0px;
 	}
+`
+const ShowTradingBtn = styled(Box)`
+  position: fixed;
+  bottom: 48px;
+  width: 95%;
+  height: 36px;
+  color: #fff;
+	border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  cursor: pointer;
+  border: solid 1px ${(props) => props.theme.basis.portGore};
+  background: ${(props) => props.theme.basis.royalPurple};
+	&:hover {
+		background: ${(props) => props.theme.basis.royalPurple};
+    border: solid 1px ${(props) => props.theme.basis.melrose};
+  }
 `
 
 export default AssetPage
