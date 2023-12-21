6db8f9d0cfde026b5db9b8cea64b85d80894b9ea
brian <quantum34@protonmail.com>
2023-12-21 17:44:16 +0900

    upgrade version

--- src/components/Common/CommonButtons.tsx
+++ src/components/Common/CommonButtons.tsx
@@ -1,4 +1,4 @@
-import { styled } from '@mui/system'
+import { styled } from '@mui/material/styles'
 import { Box } from '@mui/material'
 import CloseIcon from 'public/images/close-round.svg'
 import Image from 'next/image'
--- src/containers/Points/MyPointStatus.tsx
+++ src/containers/Points/MyPointStatus.tsx
@@ -1,4 +1,4 @@
-import { styled } from '@mui/system'
+import { styled } from '@mui/material/styles'
 import { Box, Button, Stack, Theme, Typography, useMediaQuery } from '@mui/material'
 import InfoTooltip from '~/components/Common/InfoTooltip'
 import { TooltipTexts } from '~/data/tooltipTexts'
