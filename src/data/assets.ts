b5237200d9d01b3358a7bab1a7245143d1af0f2d
Aron Yeager <aronyeager@protonmail.com>
2024-01-15 17:52:46 -0500

    Update asset descriptions

--- src/data/assets.ts
+++ src/data/assets.ts
@@ -104,6 +104,10 @@ export const collateralMapping = (index: number) => {
 export const ASSETS_DESC = [
     {
         ticker: 'arbitrum',
-        desc: "clARB is a cloned version of Arbitrum (ARB) on Solana, a groundbreaking Layer 2 scaling solution for Ethereum. By enabling faster transactions and reduced costs, Arbitrum significantly elevates Ethereum's performance. Through clARB, traders on Solana can tap into the technological advancements and efficiencies of Arbitrum, one of Ethereum's most successful Layer 2 platforms. This cloned asset not only provides a unique opportunity to engage with Ethereum's evolving ecosystem but also offers the benefits of Solana's high-speed and low-cost blockchain infrastructure."
+        desc: "clARB represents the cloned asset of Arbitrum (ARB) on Solana, showcasing a sophisticated Layer 2 scaling solution for Ethereum. Arbitrum, known for its efficient multi-round fraud detection and flexible Arbitrum Virtual Machine (AVM), offers enhanced security and a wide range of application support. clARB brings the robustness of Arbitrum's Ethereum scaling solution to Solana traders, merging Ethereum's expansive DeFi capabilities with Solana's rapid and cost-effective blockchain."
+    },
+    {
+        ticker: 'optimism',
+        desc: "clOP is the cloned asset of Optimism (OP), a key Layer 2 solution that enhances Ethereum's scalability. Known for its single-round fraud proof system and high transaction speeds, Optimism ensures swift and cost-effective Ethereum transactions. clOP offers Solana traders a unique opportunity to trade OP natively, benefiting from the efficiencies of both the Optimism and Solana ecosystems."
     },
 ]
