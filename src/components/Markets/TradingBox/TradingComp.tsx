06a9af53509c3696f55dfad7e0d39ee08d1168e9
brian <quantum34@protonmail.com>
2024-01-23 17:52:52 +0900

    fix core-702

--- src/components/Markets/TradingBox/TradingComp.tsx
+++ src/components/Markets/TradingBox/TradingComp.tsx
@@ -258,7 +258,7 @@ const TradingComp: React.FC<Props> = ({ assetIndex, slippage, onShowOption, onSh
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
                           const usdiAmt = parseFloat(event.currentTarget.value)
                           // console.log('d', event.currentTarget.value)
-                          field.onChange(event.currentTarget.value)
+                          field.onChange(usdiAmt)
                           calculateTotalAmountByFrom(usdiAmt)
                         }}
                         onMax={(balance: number) => {
@@ -296,7 +296,7 @@ const TradingComp: React.FC<Props> = ({ assetIndex, slippage, onShowOption, onSh
                         ticker={assetData?.tickerSymbol!}
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
                           const iassetAmt = parseFloat(event.currentTarget.value)
-                          field.onChange(event.currentTarget.value)
+                          field.onChange(iassetAmt)
                           calculateTotalAmountByFrom(iassetAmt)
                         }}
                         onMax={(balance: number) => {
