fc8915b49a3f612980f34c551f8c77b3dea5cfb3
brian <brian@initiumlabs.org>
2024-02-29 11:11:59 +0900

    hotfix

--- src/data/assets.ts
+++ src/data/assets.ts
@@ -27,7 +27,7 @@ export const DEFAULT_ASSET_ID = AssetTickers.arbitrum
 export const DEFAULT_ASSET_LINK = "/trade/arbitrum"
 
 //@MEMO: to add more asset, need to adjust here
-export const MAX_POOLS_FOR_SHOW = 3
+export const MAX_POOLS_FOR_SHOW = 2
 
 export const ASSETS = [
     {
