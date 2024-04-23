701878f33e9bd85a441b212eba599b634fba68a5
brian <brian@initiumlabs.org>
2024-04-23 09:50:39 +0900

    update error msg

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -3,6 +3,7 @@ import { Container, Stack, Box, Typography } from '@mui/material'
 import { StyledSection } from './page';
 import { FailedStatusBox } from '~/components/Common/TransactionStateSnackbar';
 import SupportDiscordIcon from 'public/images/support-button-discord.svg'
+import WarningMarkIcon from 'public/images/warning-mark.svg'
 import Image from 'next/image';
 
 export default function GlobalError({
@@ -23,8 +24,8 @@ export default function GlobalError({
         <StyledSection sx={{ color: '#fff' }}>
           <Container>
             <Stack width='300px' direction='column' alignItems='center' justifyContent='center' margin='0 auto' mt='130px'>
-              <Box mb='10px'><Typography fontSize='70px' fontWeight={600} color='#fff'>{':('}</Typography></Box>
-              <Typography variant="p_lg" textAlign='center'>{`We’re sorry, an unexpected error has occurred. If the error persists after reloading, please join us on Discord for support.`}</Typography>
+              <Box mb='10px'><Image src={WarningMarkIcon} alt='warning' /></Box>
+              <Typography variant="p_lg" textAlign='center'>{`Apologies for the inconvenience! Our team is currently working on an update to enhance your experience.`}</Typography>
               <a href="https://discord.gg/BXAeVWdmmD" target='_blank' rel="noreferrer"><FailedStatusBox width='74px' mt='15px'><Image src={SupportDiscordIcon} alt='discord' /> <Typography variant='p'>Discord</Typography></FailedStatusBox></a>
             </Stack>
           </Container>
