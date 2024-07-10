e01b5faa0920038f2b81b8523457cc8049914b5e
brian <brian@initiumlabs.org>
2024-07-10 09:17:10 +0900

    connect drift db

--- functions/get-users-all-bonus/get-users-all-bonus.ts
+++ functions/get-users-all-bonus/get-users-all-bonus.ts
@@ -18,12 +18,11 @@ export const handler: Handler = async (event, context) => {
   let { data: data2 } = await rpcCall2;
   // console.log('data2', data2)
 
-  //@TODO: get drift stakers
-  // const rpcCall3 = supabase.from(
-  //   "drift_stakers_top_100_cloners"
-  // ).select().eq('user_address', params.userAddress);
-  // let { data: data3 } = await rpcCall3;
-  let data3 = []
+  const rpcCall3 = supabase.from(
+    "drift_stakers_top_100_cloners"
+  ).select().lte('rank', 100);
+  let { data: data3 } = await rpcCall3;
+  console.log('data3', data3)
 
   if (error !== null) {
     console.log(error)
